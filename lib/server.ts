2657f5b1cd21690a09ce50543acaa0a1d9cdf9d6
ejson03 <elvisejsondsouza57@gmail.com>
2021-05-17 15:22:55 +0530

    synced with medical anlytica

--- lib/server.ts
+++ lib/server.ts
@@ -1,43 +1,62 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import session from 'express-session';
 import MongoStore from 'connect-mongo';
 import cors from 'cors';
 import router from './routes';
+import flash from 'connect-flash';
+
+import { Vault } from './services/vault';
 
 import * as config from './config';
+import PassportModelsGenerate from './services/Passport';
+import passport from 'passport';
 
-const app = express();
+import * as Mongo from './services/mongo';
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+async function App() {
+   const app = express();
+   app.use(express.static('./public'));
+   app.set('views', './views');
+   app.set('view engine', 'ejs');
+   app.engine('.html', require('ejs').renderFile);
 
-const MongoStoreSession = MongoStore(session);
+   app.use(express.json());
+   app.use(express.urlencoded({ extended: true }));
+   app.use(cors());
+   app.use(flash());
 
-app.use(
-   session({
-      store: new MongoStoreSession({
-         url: config.MONGO_URL
-      }),
-      secret: config.SECRET,
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-         maxAge: 1000 * 60 * 60 * 24
-      }
+   await Mongo.Connect();
+
+   if (Mongo.client != null)
+      app.use(
+         session({
+            store: MongoStore.create({ client: Mongo.client, dbName: 'session-db' }),
+
+            secret: config.SECRET,
+            resave: true,
+            saveUninitialized: true,
+            cookie: {
+               maxAge: 1000 * 60 * 60 * 24
+            }
+         })
+      );
+   app.use(passport.initialize());
+   app.use(passport.session());
+   await PassportModelsGenerate();
+
+   app.use('/', router);
+   app.get('/dashboard', (_req, res) => {
+      res.render('dashboard.html');
+   });
+
+   await Vault.Setup();
+
+   return app;
+}
+App()
+   .then(app => {
+      app.listen(config.PORT, function () {
+         console.log(`App listening on port ${config.PORT}`);
+      });
    })
-);
-app.use(express.static('./public'));
-app.set('views', './views');
-app.set('view engine', 'ejs');
-app.engine('.html', require('ejs').renderFile);
-
-app.use('/', router);
-app.get('/dashboard', (_req, res) => {
-   res.render('dashboard.html');
-});
-
-app.listen(config.PORT, function () {
-   console.log(`App listening on port ${config.PORT}`);
-});
+   .catch(err => console.error(err));
