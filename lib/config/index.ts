4f035feb9f882b50c9fa81e5d0da17b5477ad533
ejson03 <elvisejsondsouza57@gmail.com>
2021-05-17 15:19:02 +0530

    changed config

--- lib/config/index.ts
+++ lib/config/index.ts
@@ -1,19 +1,18 @@
-import dotenv from 'dotenv';
+// const dotenv = require('dotenv');
 
-dotenv.config();
+// dotenv.config();
 
-export const PORT: number = Number(process.env.PORT || 8000);
-export const SECRET: string = String(process.env.SECRET);
+export const PORT: number = Number(process.env.PORT || 5000);
+export const SECRET: string = String(process.env.SECRET || 'secret_default');
 export const JWT_SECRET: string = String(process.env.JWT_SECRET);
 export const VAULT: any = {
-   url: String(process.env.VAULT_URL),
-   token: String(process.env.VAULT_TOKEN)
+   url: String(process.env.VAULT_URL || 'http://localhost:8200' ),
+   token: String(process.env.VAULT_TOKEN || 'myroot')
 };
 export const MONGO_URL: string = String(process.env.MONGO_URL || 'mongodb://localhost:27017/');
 export const RASA_URL: string = String(process.env.RASA_URL || 'http://localhost:5005');
-export const BIGCHAIN_URL: string = String(process.env.BIGCHAIN_URL || 'http://localhost:9984/api/v1');
+export const BIGCHAIN_URL: string = String(process.env.BIGCHAIN_URL || 'http://localhost:9984/api/v1/');
 export const IPFS: any = {
    url: String(process.env.IPFS_URL || 'ipfs.infura.io'),
    port: String('5001')
 };
-export const GOOGLE_MAPS_KEY: string = String(process.env.GOOGLE_MAPS_KEY);
